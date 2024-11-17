60091f1637a457a3a1d86ca768bcef2973fd8155
nithi guna <nithimadhavguna@gmail.com>
2024-11-17 17:45:17 -0600

    BaseClass

--- src/test/java/com/DsAlgo/testBase/BaseClass.java
+++ src/test/java/com/DsAlgo/testBase/BaseClass.java
@@ -1,10 +1,15 @@
 package com.DsAlgo.testBase;
 
+import java.io.File;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -12,27 +17,27 @@ import org.testng.annotations.Parameters;
 
 import com.DsAlgo.utilities.ConfigFileReader;
 
+
 public class BaseClass {
 
-	public static WebDriver driver;
+	public WebDriver driver;
 
 	ConfigFileReader configFileReader = ConfigFileReader.getInstance();
 
 	@BeforeMethod
-//	@BeforeClass
 	@Parameters("browser")
 	public void setup(@Optional("Chrome") String browser) {
 		if (browser.equals("Chrome")) {
-			System.out.println("Chrome Browser = " + browser);
 			driver = new ChromeDriver();
 		} else if (browser.equals("Edge")) {
-			System.out.println("Edge Browser = " + browser);
 			driver = new EdgeDriver();
-		} else {
-			System.out.println("Default Browser = " + browser);
+			
+		} else if (browser.equals("Firefox")) {
+			driver = new FirefoxDriver();
+		}else {
 			driver = new ChromeDriver();
 		}
-
+		System.out.println("The BaseClass driver : " +driver);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(configFileReader.getHomeUrl());
@@ -50,8 +55,30 @@ public class BaseClass {
 //	            System.out.println("Test failed: " + result.getName());
 //	            // Save or log the screenshot here if needed
 //	        }
-//	      
+		
 		driver.quit();
+		
+		 
 	}
+	
+	public WebDriver getDriver() {
+        return driver;
+    }
+	
+	public String getScreenshot(String testName, WebDriver driver)
+    {
+		
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        String screenshotPath = "target/screenshots" + testName +".png";
+        try {
+            FileUtils.copyFile(screenshot,new File(screenshotPath));
+        } catch (Exception e) {
+            System.out.println("Takes Screenshot is null");
+            e.printStackTrace();
+        }
+        return screenshotPath;
+
+    }
 
 }
