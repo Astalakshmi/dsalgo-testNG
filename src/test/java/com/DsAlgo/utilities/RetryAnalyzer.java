888d734ece3d953605bd9b4ea83808d36c93ce1f
Sandhya Sankaran <svsandhya23@gmail.com>
2024-11-19 18:29:51 -0800

    updated screenshots for retry

--- src/test/java/com/DsAlgo/utilities/RetryAnalyzer.java
+++ src/test/java/com/DsAlgo/utilities/RetryAnalyzer.java
@@ -1,33 +1,48 @@
 package com.DsAlgo.utilities;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
+
+import com.DsAlgo.testBase.BaseClass;
+import com.aventstack.extentreports.Status;
+
+import io.qameta.allure.Allure;
 public class RetryAnalyzer implements IRetryAnalyzer {
-	   private static final int    maxTry = 1;
-	   private              int    count  = 0;
-	 @Override
-	public boolean retry (final ITestResult iTestResult) {
-	       if (!iTestResult.isSuccess ()) {
-	           if (this.count < maxTry) {
-	               LoggerLoad.info ("Retrying test " + iTestResult.getName () + " with status " + getResultStatusName (
-	                   iTestResult.getStatus ()) + " for the " + (this.count + 1) + " time(s).");
-	               this.count++;
-	               return true;
-	           }
-	       }
-	       return false;
-	 }
-	 public String getResultStatusName (final int status) {
-	       String resultName = null;
-	       if (status == 1) {
-	           resultName = "SUCCESS";
-	       }
-	       if (status == 2) {
-	           resultName = "FAILURE";
-	       }
-	       if (status == 3) {
-	           resultName = "SKIP";
-	       }
-	       return resultName;
-	   }
+	   private static final int  maxTry = 3;
+	   private int count  = 0;
+	    @Override
+	    public boolean retry(ITestResult iTestResult) {
+	        if (!iTestResult.isSuccess()) {                     //Check if test not succeed
+	            if (count < maxTry) {                           //Check if maxTry count is reached
+	                count++;                                    //Increase the maxTry count by 1
+	                iTestResult.setStatus(ITestResult.FAILURE); //Mark test as failed and take base64Screenshot
+	                failOperations(iTestResult);   //ExtentReports fail operations
+	                return true;                                //Tells TestNG to re-run the test
+	            }
+	        } else {
+	            iTestResult.setStatus(ITestResult.SUCCESS);     //If test passes, TestNG marks it as passed
+	        }
+	        return false;
+	    }
+	    public void failOperations(ITestResult iTestResult) {
+	        Object testClass = iTestResult.getInstance();
+	        WebDriver driver = ((BaseClass) testClass).driver;
+	        String base64Screenshot = "data:image/png;base64," + ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+	        ExtentTestManager.getTest().log(Status.FAIL, "Test Failed",
+	        		ExtentTestManager.getTest().addScreenCaptureFromBase64String(base64Screenshot).getModel().getMedia().get(0));
+	        File screenshotAsFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			try {
+				
+				Allure.addAttachment("screenshot", FileUtils.openInputStream(screenshotAsFile));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    }
 		
 	}
