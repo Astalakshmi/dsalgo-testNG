1b38ea167c2bd6c53dfdad7c7d56db4dd5e68a95
Sandhya Sankaran <svsandhya23@gmail.com>
2024-11-19 18:32:50 -0800

    removed unwanted imports

--- src/test/java/com/DsAlgo/testBase/BaseClass.java
+++ src/test/java/com/DsAlgo/testBase/BaseClass.java
@@ -1,11 +1,7 @@
 package com.DsAlgo.testBase;
 
-import java.io.File;
 import java.time.Duration;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
